0be8c33deaa380a3ab5596fc9b05fed7b76ec968
DasunWiduranga <dasun.20200233@iit.ac.lk>
2023-04-26 18:01:20 +0530

    done

---
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Profile;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateAvatarRequest;
+
+class AvatarController extends Controller
+{
+    public function update(UpdateAvatarRequest $request){
+
+      
+        $path = $request -> file('avatar') -> store ('avatars', 'public');
+        auth() ->user() ->update(['avatar' => $path]);
+       
+        return redirect(route('profile.edit')) -> with('message', 'Avatar is Updated');
+    }
+}
